79bb5532d2faec51321c8cdd8262b15231bed17b
afiahana <54826188+afiahana@users.noreply.github.com>
2020-05-20 14:05:46 +0700

    Add files via upload

---
+++ Sequences/Sequence5_AfiaHanaYusriya_0111.sql
@@ -0,0 +1,13 @@
+/*
+    Generate id_donatur secara otomatis berurutan saat penambahan data baru
+
+    Afia Hana Yusriya
+    05111840000111
+    Manajemen Basis Data 2020
+*/
+
+/*SEQUENCE*/
+CREATE SEQUENCE SEQ_DONATUR
+START WITH 281
+INCREMENT BY 1
+MAXVALUE 999;
