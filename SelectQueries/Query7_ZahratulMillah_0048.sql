556938b75bc81b9ccb9cb5e50ee851c5b35407cc
hipzi <zahramillah582@gmail.com>
2020-05-20 14:49:29 +0700

    0048

--- SelectQueries/Query7_ZahratulMillah_0048.sql
+++ SelectQueries/Query7_ZahratulMillah_0048.sql
@@ -9,3 +9,13 @@
 SELECT *  FROM donatur d JOIN donasi_uang du ON d.id_donatur = du.id_donatur 
     JOIN jenis_bank j ON du.id_bank = j.id_bank 
     ORDER BY d.id_donatur ASC;
+
+/*
+Agar hanya mengambil 10 data pertama saat melakukan pengujian
+
+    SELECT e.nama_event,SUM(du.nominal) total_nominal
+        FROM event e,donasi_uang du 
+        WHERE e.id_event = du.id_event
+        GROUP BY e.id_event
+        fetch first 10 rows only;
+*/
